08570eb8eda597a036e5ee84eabdc44f064f558b
haraoka <haraoka@screen.co,jp>
2023-03-10 14:24:11 +0900

    Add OutOfSampleCausalTuning

---
+++ lingam/experimental/__init__.py
@@ -0,0 +1,10 @@
+"""
+Python implementation of the LiNGAM algorithms.
+The LiNGAM Project: https://sites.google.com/view/sshimizu06/lingam
+"""
+
+from .oct import OutOfSampleCausalTuning
+
+__all__ = [
+    "OutOfSampleCausalTuning",
+]
