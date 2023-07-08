0cb2c6da7086c21736a1ae562272238bce898ced
ikeuchi-screen <ikeuchi@screen.co.jp>
2023-07-08 07:12:34 +0000

    Change python version

--- setup.py
+++ setup.py
@@ -32,5 +32,5 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
 )
