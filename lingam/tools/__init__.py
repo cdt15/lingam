a9889fa09b683334dbb5c7f314ccd7ecde87e2fc
haraoka-screen <haraoka@screen.co,jp>
2024-08-27 19:51:37 +0900

    Modify to customize cd_model

--- lingam/tools/__init__.py
+++ lingam/tools/__init__.py
@@ -3,8 +3,9 @@ Python implementation of the LiNGAM algorithms.
 The LiNGAM Project: https://sites.google.com/view/sshimizu06/lingam
 """
 import numbers
-import numpy as np
+from abc import ABCMeta, abstractmethod
 
+import numpy as np
 from sklearn.utils import check_array, check_scalar, check_random_state
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
@@ -13,6 +14,7 @@ from ..multi_group_direct_lingam import MultiGroupDirectLiNGAM
 
 __all__ = [
     "bootstrap_with_imputation",
+    "BaseMultiGroupCDModel",
 ]
 
 
@@ -20,6 +22,7 @@ def bootstrap_with_imputation(
     X,
     n_sampling,
     n_repeats,
+    cd_model=None,
     prior_knowledge=None,
     apply_prior_knowledge_softly=False,
     random_state=None
@@ -42,8 +45,12 @@ def bootstrap_with_imputation(
         Number of bootstraps.
     n_repeats : int
         Number of times to complete missing values for each bootstrap sample.
+    cd_model : object, optional (default=None)
+        Instance of a class inheriting from BaseMultiGroupCDModel class.
+        If None, use MultiGroupDirectLiNGAM to estimate causal order.
     prior_knowledge : array-like, shape (n_features, n_features), optional (default=None)
         Prior knowledge used for causal discovery, where ``n_features`` is the number of features.
+        prior_knowledge is used only if cd_model is None.
 
         The elements of prior knowledge matrix are defined as follows:
 
@@ -52,6 +59,7 @@ def bootstrap_with_imputation(
         * ``-1`` : No prior knowledge is available to know if either of the two cases above (0 or 1) is true.
     apply_prior_knowledge_softly : boolean, optional (default=False)
         If True, apply prior knowledge softly.
+        apply_prior_knowledge_softly is used only if cd_model is None.
     random_state : int, optional (default=None)
         ``random_state`` is the seed used by the random number generator.
 
@@ -78,15 +86,32 @@ def bootstrap_with_imputation(
         min_val=1
     )
 
+    n_repeats = check_scalar(
+        n_repeats,
+        "n_repeats",
+        (numbers.Integral, np.integer),
+        min_val=1
+    )
+
+    if cd_model is not None and not isinstance(cd_model, BaseMultiGroupCDModel):
+        raise ValueError("cd_model must be an instance of a subclass of BaseMultiGroupCDModel.")
+
+    n_samples, n_features = X.shape
+    if prior_knowledge is not None:
+        prior_knowledge = check_array(prior_knowledge)
+        if prior_knowledge.shape != (n_features, n_features):
+            raise ValueError("The shape of prior_knowledge must be (n_features, n_features).")
+
     random_state = check_random_state(random_state)
 
     imp = IterativeImputer(sample_posterior=True, random_state=random_state)
 
-    cd_model = MultiGroupDirectLiNGAM(
-        prior_knowledge=prior_knowledge,
-        apply_prior_knowledge_softly=apply_prior_knowledge_softly,
-        random_state=random_state
-    )
+    if cd_model is None:
+        cd_model = _DefaultMultiGroupCDModel(
+            prior_knowledge=prior_knowledge,
+            apply_prior_knowledge_softly=apply_prior_knowledge_softly,
+            random_state=random_state
+        )
 
     resampled_indices = []
     causal_orders = []
@@ -98,6 +123,9 @@ def bootstrap_with_imputation(
         resampled_index = random_state.choice(np.arange(X.shape[0]), replace=True, size=len(X))
         bootstrap_sample = X[resampled_index]
 
+        # send bootstrap_sample that has not been imputed yet
+        cd_model.before_imputation(bootstrap_sample)
+
         # make datasets
         datasets = []
         for i in range(n_repeats):
@@ -105,7 +133,7 @@ def bootstrap_with_imputation(
             datasets.append(dataset)
 
         # run causal discovery assuming a common causal structure
-        cd_model.fit(datasets)
+        causal_order, adjacency_matrices = cd_model.fit(datasets)
 
         # store imputation results
         # hold values only if NaN
@@ -115,8 +143,8 @@ def bootstrap_with_imputation(
         imputation_result[:, pos] = datasets[:, pos]
 
         resampled_indices.append(resampled_index)
-        causal_orders.append(cd_model.causal_order_)
-        adj_matrices_list.append(cd_model.adjacency_matrices_)
+        causal_orders.append(causal_order)
+        adj_matrices_list.append(adjacency_matrices)
         imputation_results.append(imputation_result)
 
     resampled_indices = np.array(resampled_indices)
@@ -125,3 +153,61 @@ def bootstrap_with_imputation(
     imputation_results = np.array(imputation_results)
 
     return causal_orders, adj_matrices_list, resampled_indices, imputation_results
+
+
+class BaseMultiGroupCDModel(metaclass=ABCMeta):
+
+    @abstractmethod
+    def before_imputation(self, X):
+        """
+        This method is called just before the bootstrap sample is imputed.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            ``X`` is a bootstrap sample and has not yet been imputed.
+            ``n_samples`` is the number of samples
+            and ``n_features`` is the number of features.
+
+        Returns
+        -------
+        None
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def fit(self, X_list):
+        """
+        This method is called to fit imputed bootstrap sample.
+
+        Parameters
+        ----------
+        X_list : list, shape [X, ...]
+            ``X_list`` is a list of imputed bootstrap samples.
+            Multiple datasets for training, where ``X`` is an dataset.
+            The shape of ''X'' is (n_samples, n_features),
+            where ``n_samples`` is the number of samples and ``n_features`` is the number of features.
+
+        Returns
+        -------
+        causal_order : array-like
+        adjacenyc_matrices : array-like
+        """
+        raise NotImplementedError
+
+
+class _DefaultMultiGroupCDModel(BaseMultiGroupCDModel):
+
+    def __init__(self, prior_knowledge=None, apply_prior_knowledge_softly=False, random_state=None):
+        self._model = MultiGroupDirectLiNGAM(
+            prior_knowledge=prior_knowledge,
+            apply_prior_knowledge_softly=apply_prior_knowledge_softly,
+            random_state=random_state
+        )
+
+    def before_imputation(self, X):
+        pass
+
+    def fit(self, X_list):
+        self._model.fit(X_list)
+        return self._model.causal_order_, self._model.adjacency_matrices_
