8eb91a4225838ec59c2bdf03d3c1dd0e9515e38a
ide-screen <ide@screen.co.jp>
2025-06-16 12:02:14 +0900

    Add prior_knowledge option

--- tests/test_resit.py
+++ tests/test_resit.py
@@ -78,3 +78,291 @@ def test_fit_invalid():
         pass
     else:
         raise AssertionError
+
+
+def test_bootstrap_success():
+    # causal direction: x0 --> x1 --> x3
+    x0 = np.random.uniform(size=1000)
+    x1 = 2.0 * x0 + np.random.uniform(size=1000)
+    x2 = np.random.uniform(size=1000)
+    x3 = 4.0 * x1 + np.random.uniform(size=1000)
+    X = pd.DataFrame(np.array([x0, x1, x2, x3]).T, columns=["x0", "x1", "x2", "x3"])
+
+    reg = RandomForestRegressor(max_depth=4, random_state=0)
+    model = RESIT(regressor=reg)
+    result = model.bootstrap(X, n_sampling=5)
+    am = result.adjacency_matrices_
+    assert len(am) == 5
+    te = result.total_effects_
+    assert len(te) == 5
+
+    # No argument
+    cdc = result.get_causal_direction_counts()
+
+    # n_directions=2
+    cdc = result.get_causal_direction_counts(n_directions=2)
+
+    # min_causal_effect=0.2
+    cdc = result.get_causal_direction_counts(min_causal_effect=0.2)
+
+    # split_by_causal_effect_sign=True
+    cdc = result.get_causal_direction_counts(split_by_causal_effect_sign=True)
+
+    # No argument
+    dagc = result.get_directed_acyclic_graph_counts()
+
+    # n_dags=2
+    dagc = result.get_directed_acyclic_graph_counts(n_dags=2)
+
+    # min_causal_effect=0.6
+    dagc = result.get_directed_acyclic_graph_counts(min_causal_effect=0.6)
+
+    # split_by_causal_effect_sign=True
+    dagc = result.get_directed_acyclic_graph_counts(split_by_causal_effect_sign=True)
+
+    # get_probabilities
+    probs = result.get_probabilities()
+
+    # get_probabilities
+    probs = result.get_probabilities(min_causal_effect=0.6)
+
+    # get_total_causal_effects
+    ce = result.get_total_causal_effects()
+
+    # get_total_causal_effects
+    ce = result.get_total_causal_effects(min_causal_effect=0.6)
+
+    # get_paths
+    paths = result.get_paths(1, 2)
+
+
+def test_bootstrap_invalid_data():
+    # causal direction: x0 --> x1 --> x3
+    x0 = np.random.uniform(size=1000)
+    x1 = 2.0 * x0 + np.random.uniform(size=1000)
+    x2 = np.random.uniform(size=1000)
+    x3 = 4.0 * x1 + np.random.uniform(size=1000)
+    X = pd.DataFrame(np.array([x0, x1, x2, x3]).T, columns=["x0", "x1", "x2", "x3"])
+
+    reg = RandomForestRegressor(max_depth=4, random_state=0)
+
+    # Invalid argument: bootstrap(n_sampling=-1)
+    model = RESIT(regressor=reg)
+    try:
+        result = model.bootstrap(X, n_sampling=-1)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError
+
+    # Invalid argument: bootstrap(n_sampling='3')
+    model = RESIT(regressor=reg)
+    try:
+        result = model.bootstrap(X, n_sampling='3')
+    except ValueError:
+        pass
+    else:
+        raise AssertionError
+
+    # Invalid argument: get_causal_direction_counts(n_directions=-1)
+    model = RESIT(regressor=reg)
+    result = model.bootstrap(X, n_sampling=5)
+    try:
+        result.get_causal_direction_counts(n_directions=-1)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError
+
+    # Invalid argument: get_causal_direction_counts(n_directions='3')
+    try:
+        result.get_causal_direction_counts(n_directions='3')
+    except ValueError:
+        pass
+    else:
+        raise AssertionError
+
+    # Invalid argument: get_causal_direction_counts(min_causal_effect=-1.0)
+    try:
+        result.get_causal_direction_counts(min_causal_effect=-1.0)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError
+
+    # Invalid argument: get_directed_acyclic_graph_counts(n_dags=-1)
+    try:
+        result.get_directed_acyclic_graph_counts(n_dags=-1)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError
+
+    # Invalid argument: get_directed_acyclic_graph_counts(n_dags='3')
+    try:
+        result.get_directed_acyclic_graph_counts(n_dags='3')
+    except ValueError:
+        pass
+    else:
+        raise AssertionError
+
+    # Invalid argument: get_directed_acyclic_graph_counts(min_causal_effect=-1.0)
+    try:
+        result.get_directed_acyclic_graph_counts(min_causal_effect=-1.0)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError
+
+    # Invalid argument: get_probabilities(min_causal_effect=-1.0)
+    try:
+        result.get_probabilities(min_causal_effect=-1.0)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError
+
+    # Invalid argument: get_total_causal_effects(min_causal_effect=-1.0)
+    try:
+        result.get_total_causal_effects(min_causal_effect=-1.0)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError
+
+    # Invalid argument: get_paths(min_causal_effect=-1.0)
+    try:
+        result.get_paths(0, 1, min_causal_effect=-1.0)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError
+
+
+def test_prior_knowledge_success():
+    # causal direction: x0 --> x1 --> x3
+    x0 = np.random.uniform(size=1000)
+    x1 = 2.0 * x0 + np.random.uniform(size=1000)
+    x2 = np.random.uniform(size=1000)
+    x3 = 4.0 * x1 + np.random.uniform(size=1000)
+    X = pd.DataFrame(np.array([x0, x1, x2, x3]).T, columns=["x0", "x1", "x2", "x3"])
+
+    reg = RandomForestRegressor(max_depth=4, random_state=0)
+
+    # prior knowledge: nothing
+    pk = np.array(
+        [
+            [0, -1, -1, -1],
+            [-1, 0, -1, -1],
+            [-1, -1, 0, -1],
+            [-1, -1, -1, 0],
+        ]
+    )
+    model = RESIT(regressor=reg, prior_knowledge=pk)
+    model.fit(X)
+    co = model.causal_order_
+    assert co.index(0) < co.index(1) < co.index(3)
+
+    # prior knowledge: x1 is exogenous
+    pk = np.array(
+        [
+            [0, -1, -1, -1],
+            [0, 0, 0, 0],
+            [-1, -1, 0, -1],
+            [-1, -1, -1, 0],
+        ]
+    )
+    model = RESIT(regressor=reg, prior_knowledge=pk)
+    model.fit(X)
+    co = model.causal_order_
+    assert co.index(1) < co.index(0)
+    assert co.index(1) < co.index(2)
+    assert co.index(1) < co.index(3)
+
+    # prior knowledge: x0 is sink
+    pk = np.array(
+        [
+            [0, -1, -1, -1],
+            [0, 0, -1, -1],
+            [0, -1, 0, -1],
+            [0, -1, -1, 0],
+        ]
+    )
+    model = RESIT(regressor=reg, prior_knowledge=pk)
+    model.fit(X)
+    co = model.causal_order_
+    assert co.index(0) > co.index(1)
+    assert co.index(0) > co.index(2)
+    assert co.index(0) > co.index(3)
+
+    # prior knowledge: x2-->x3 has path
+    pk = np.array(
+        [
+            [0, -1, -1, -1],
+            [-1, 0, -1, -1],
+            [-1, -1, 0, 0],
+            [-1, -1, 1, 0],
+        ]
+    )
+    model = RESIT(regressor=reg, prior_knowledge=pk)
+    model.fit(X)
+    co = model.causal_order_
+    assert co.index(2) < co.index(3)
+
+    # prior knowledge: x1-->x3 does not have path
+    pk = np.array(
+        [
+            [0, -1, -1, -1],
+            [-1, 0, -1, -1],
+            [-1, -1, 0, -1],
+            [-1, 0, -1, 0],
+        ]
+    )
+    model = RESIT(regressor=reg, prior_knowledge=pk)
+    model.fit(X)
+    co = model.causal_order_
+
+
+def test_prior_knowledge_invalid():
+    # causal direction: x0 --> x1 --> x3
+    x0 = np.random.uniform(size=1000)
+    x1 = 2.0 * x0 + np.random.uniform(size=1000)
+    x2 = np.random.uniform(size=1000)
+    x3 = 4.0 * x1 + np.random.uniform(size=1000)
+    X = pd.DataFrame(np.array([x0, x1, x2, x3]).T, columns=["x0", "x1", "x2", "x3"])
+
+    reg = RandomForestRegressor(max_depth=4, random_state=0)
+
+    # prior knowledge: invalid
+    pk = np.array(
+        [
+            [0, -1, 1],
+            [-1, 0, -1],
+            [-1, -1, 0],
+            [-1, -1, -1],
+        ]
+    )
+    try:
+        model = RESIT(regressor=reg, prior_knowledge=pk)
+        model.fit(X)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError
+
+    # prior knowledge: inconsistent
+    pk = np.array(
+        [
+            [0, 1, -1, -1],
+            [1, 0, -1, -1],
+            [-1, -1, 0, -1],
+            [-1, -1, -1, 0],
+        ]
+    )
+    try:
+        model = RESIT(regressor=reg, prior_knowledge=pk)
+        model.fit(X)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError
