e6b1f02343cdfc7db9c3d765b7dcf7cbf8bd523f
Takashi Ikeuchi <54339040+ikeuchi-screen@users.noreply.github.com>
2025-06-18 13:48:49 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,5 +34,5 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.9',
 )
